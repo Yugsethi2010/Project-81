de167399bba95de36c4389d8e7039ee4c109d887
Yugsethi2010 <94779614+Yugsethi2010@users.noreply.github.com>
2021-11-21 13:50:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+canvas =
+ document.getElementById("myCanvas");
+    ctx= canvas.getContext("2d");
+    
+
+    ctx.beginPath();
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = 1;
+    ctx.rect("150", "143", "430", "200");
+    ctx.stroke();
+
+    
+    
+
+    ctx.beginPath();
+    ctx.strokeStyle = "blue";
+    ctx.lineWidth = 1;
+    ctx.rect("150", "143", "430", "200");
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.strokeStyle = "green";
+    ctx.lineWidth = 1;
+    ctx.rect("150", "143", "430", "200");
+    ctx.stroke();
+
+   ctx.beginPath();
+    ctx.strokeStyle = "yellow";
+    ctx.lineWidth = 1;
+    ctx.rect("150", "143", "430", "200");
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = 1;
+    ctx.rect("150", "143", "430", "200");
+    ctx.stroke();
